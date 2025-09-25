48d725a33c514a8412b65f1554b530c90d1ca32e
Reve Chasseur <reve@djhuntress.me>
2025-09-25 21:18:25 +0100

    Add quick connect DTO for ease of serialise/deserialise

---
+++ Dto/Server/QuickConnectDto.cs
@@ -0,0 +1,20 @@
+﻿using LaciSynchroni.Common.Data.Enum;
+using MessagePack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaciSynchroni.Common.Dto.Server;
+
+[MessagePackObject()]
+public record QuickConnectDto
+{
+    [Key(0)]
+    public string ServerName = string.Empty;
+    [Key(1)]
+    public string ServerURI = string.Empty;
+    [Key(2)]
+    public string SecretKey = string.Empty;
+}
