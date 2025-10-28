47b7cdd7d93fd5674ea30c69edf229ad3da2321f
Reve Chasseur <reve@djhuntress.me>
2025-10-28 22:15:21 +0000

    Add common data for the plugin HTTP server

---
+++ Data/PluginHTTPServerData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaciSynchroni.Common.Data;
+public static class PluginHTTPServerData
+{
+    public const string Address = "http://localhost:42069";
+}
