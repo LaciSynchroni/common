7e26f0c967f071beed71113391bc03b7be77007a
Reve Chasseur <reve@djhuntress.me>
2025-10-29 23:48:49 +0000

    Remove the server URI from the config DTO - we can't get this unless we contact the server

--- Dto/Server/ConfigurationDto.cs
+++ Dto/Server/ConfigurationDto.cs
@@ -9,7 +9,6 @@ public record ConfigurationDto
 {
     public string ServerName { get; set; } = string.Empty;
     public Version ServerVersion { get; set; } = new(0, 0, 0);
-    public Uri ServerUri { get; set; } = new("wss://noemptyuri");
     public Uri? HubUri { get; set; } = null;
     public Uri? AuthUri { get; set; } = null;
     public string? DiscordInvite { get; set; } = null;
