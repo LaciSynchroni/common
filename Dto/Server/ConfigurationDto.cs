6d4666d5ad5b948e3a0afcf874e94b4371948341
Reve Chasseur <reve@djhuntress.me>
2025-10-29 21:30:44 +0000

    Add a couple more entries to the config DTO

--- Dto/Server/ConfigurationDto.cs
+++ Dto/Server/ConfigurationDto.cs
@@ -10,6 +10,8 @@ public record ConfigurationDto
     public string ServerName { get; set; } = string.Empty;
     public Version ServerVersion { get; set; } = new(0, 0, 0);
     public Uri ServerUri { get; set; } = new("wss://noemptyuri");
+    public Uri? HubUri { get; set; } = null;
+    public Uri? AuthUri { get; set; } = null;
     public string? DiscordInvite { get; set; } = null;
     public string? ServerRules { get; set; } = null;
     public bool? IsOAuthEnabled { get; set; } = null;
