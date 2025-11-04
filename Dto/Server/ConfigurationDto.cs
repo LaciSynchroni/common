717794259c2eb87c8a5bb7e48555922746f6c2dd
Reve Chasseur <reve@djhuntress.me>
2025-11-04 21:48:04 +0000

    Remove auth uri from the config dto - this is intended for local dev, the server itself doesn't know anything about it

--- Dto/Server/ConfigurationDto.cs
+++ Dto/Server/ConfigurationDto.cs
@@ -10,7 +10,6 @@ public record ConfigurationDto
     public string ServerName { get; set; } = string.Empty;
     public Version ServerVersion { get; set; } = new(0, 0, 0);
     public Uri? HubUri { get; set; } = null;
-    public Uri? AuthUri { get; set; } = null;
     public string? DiscordInvite { get; set; } = null;
     public string? ServerRules { get; set; } = null;
     public bool? IsOAuthEnabled { get; set; } = null;
