627734c4654d35169bab386ae77fde74329e1d26
Reeve <reeve@djhuntress.me>
2025-11-19 07:08:11 -0800

    Onboarding flow rework (#3)
    * Add quick connect DTO for ease of serialise/deserialise
    
    * Remove the name from the quick connect DTO
    
    * Add whether OAuth is enabled to the configuration DTO
    
    * Add whether to bypass the version check to the config dto
    
    * Add common data for the plugin HTTP server
    
    * Rename the HTTP server data file to be consistent
    
    * Split the plugin HTTP server address in to hostname and port
    
    * Add a couple more entries to the config DTO
    
    * Remove the server URI from the config DTO - we can't get this unless we contact the server
    
    * Remove Quick Connect Dto, now we handle the flow in a better way
    
    * Remove auth uri from the config dto - this is intended for local dev, the server itself doesn't know anything about it

---
+++ Data/PluginHttpServerData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaciSynchroni.Common.Data;
+public static class PluginHttpServerData
+{
+    public const string Hostname = "http://localhost";
+    public const int Port = 42069;
+}
--- Dto/Server/ConfigurationDto.cs
+++ Dto/Server/ConfigurationDto.cs
@@ -9,7 +9,9 @@ public record ConfigurationDto
 {
     public string ServerName { get; set; } = string.Empty;
     public Version ServerVersion { get; set; } = new(0, 0, 0);
-    public Uri ServerUri { get; set; } = new("wss://noemptyuri");
+    public Uri? HubUri { get; set; } = null;
     public string? DiscordInvite { get; set; } = null;
     public string? ServerRules { get; set; } = null;
+    public bool? IsOAuthEnabled { get; set; } = null;
+    public bool? BypassVersionCheck { get; set; } = null;
 }
